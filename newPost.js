33019c432edb79c5a61dc6f4f8161d0ff0e42039
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2020-03-30 11:43:48 +0200

    Update newPost.js

--- newPost.js
+++ newPost.js
@@ -6,31 +6,21 @@ var day = date.getDate()
 var month = date.getMonth() + 1
 var year = date.getFullYear()
 var title = process.argv[2]
-var dir =
-  __dirname +
-  '/src/pages/posts/' +
-  year +
-  '/' +
-  month +
-  '/' +
-  day +
-  '/' +
-  title +
-  '/'
+var dir = __dirname + '/src/pages/posts/' + year + '/' + month + '/' + day + '/' + title + '/'
 var file = dir + title + '.md'
 
 console.log(day, month, year)
 console.log(title)
 console.log(dir)
 
-mkdirp(dir, function(err) {
-  if (err) {
-    console.error(err)
-  }
+mkdirp(dir, function (err) {
+    if (err) {
+        console.error(err)
+    }
 
-  console.log('DIRECTORY CREATED')
-  fs.writeFile(dir + title + '.md', '', function(err) {
-    if (err) throw err
-    console.log('FILE CREATED')
-  })
+    console.log('DIRECTORY CREATED')
+    fs.writeFile(file, '', function (err) {
+        if (err) throw err
+        console.log('FILE CREATED')
+    })
 })
